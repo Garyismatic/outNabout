c10b12740ec4b510c0043c477587dad53790f2e4
Gary <garyevans1501@gmail.com>
2025-06-14 23:17:57 +0100

    make get requests to open weather api

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const searchBox = document.getElementsByName("destination");
 const searchButton = document.getElementById("search-button");
 
+let city = "";
+
 const handleSearch = (e) => {
-  console.log(searchBox[0].value);
+  city = searchBox[0].value;
+  console.log(city);
+  fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((coordinates) => {
+      const { latitude, longitude } = coordinates.results[0];
+      return { latitude, longitude };
+    })
+    .then(({ latitude, longitude }) => {
+      return fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+      );
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 searchButton.addEventListener("click", handleSearch);
